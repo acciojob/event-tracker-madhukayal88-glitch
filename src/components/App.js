c08387ff04fe00dee5949afc57c348a98e352419
kayalvizhi <madhukayal88@gmail.com>
2026-08-07 10:44:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
+import React from 'react';
+import EventTracker from './EventTracker';
+import './styles.css';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app-container">
+      <EventTracker />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

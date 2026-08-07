d96f18946e804abb142d04f9cf87e7e1f77c2012
kayalvizhi <madhukayal88@gmail.com>
2026-08-07 10:49:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Popup from 'react-popup';
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <Popup />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
